e0ab8a304b1529f749381cd0f8fc2ba989d5ed7b
Lukass <fraimerdelacruzjaziel@gmail.com>
2025-01-03 03:16:08 -0400

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,8 @@
-import { Client, Events, REST, Routes, ActivityType } from 'discord.js';
-import { Logger } from '../utils/logger';
-import { reviewModel } from '../models/review';
-import fs from 'fs';
-import path from 'path';
+import { Client, Events, REST, Routes, ActivityType } from "discord.js";
+import { Logger } from "../utils/logger";
+import { reviewModel } from "../models/review";
+import fs from "fs";
+import path from "path";
 
 export const event = {
 	name: Events.ClientReady,
@@ -18,8 +18,8 @@ export const event = {
 		const statuses = [
 			{
 				type: ActivityType.Custom,
-				name: 'Simplifying reviews',
-				state: 'Simplifying reviews',
+				name: "💙 Simplifying reviews",
+				state: "💙 Simplifying reviews",
 			},
 			{
 				type: ActivityType.Watching,
@@ -27,7 +27,8 @@ export const event = {
 			},
 			{
 				type: ActivityType.Custom,
-				name: `${client.guilds.cache.size.toLocaleString()} servers`,
+				name: `🚀 ${client.guilds.cache.size.toLocaleString()} servers`,
+				state: `🚀 ${client.guilds.cache.size.toLocaleString()} servers`,
 			},
 		];
 
@@ -52,25 +53,31 @@ export const event = {
 		}, 30000);
 
 		const commands = [];
-		const commandsPath = path.join(__dirname, '..', 'commands');
-		const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.ts'));
+		const commandsPath = path.join(__dirname, "..", "commands");
+		const commandFiles = fs
+			.readdirSync(commandsPath)
+			.filter((file) => file.endsWith(".ts"));
 
 		for (const file of commandFiles) {
 			const command = require(path.join(commandsPath, file));
-			if ('data' in command) {
+			if ("data" in command) {
 				commands.push(command.data.toJSON());
 			}
 		}
 
 		try {
-			Logger.info(`Started refreshing ${commands.length} application (/) commands.`);
+			Logger.info(
+				`Started refreshing ${commands.length} application (/) commands.`
+			);
 
 			const rest = new REST().setToken(process.env.DISCORD_TOKEN!);
 			await rest.put(Routes.applicationCommands(client.application.id), {
 				body: commands,
 			});
 
-			Logger.info(`Successfully reloaded ${commands.length} application (/) commands.`);
+			Logger.info(
+				`Successfully reloaded ${commands.length} application (/) commands.`
+			);
 		} catch (error) {
 			Logger.error(`Error refreshing commands: ${error}`);
 		}
